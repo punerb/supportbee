e6e2ade3d9f30772d1bf092803d0784d27dc2271
Sandip Ransing <sandip@joshsoftware.com>
2012-06-15 19:56:35 +0530

    tickets API call added

--- lib/supportbee.rb
+++ lib/supportbee.rb
@@ -1,5 +1,20 @@
 require "supportbee/version"
+require 'httparty'
 
 module Supportbee
-  # Your code goes here...
+  class Base
+    include HTTParty
+
+    base_uri "https://josh.supportbee.com"
+
+    def initialize(auth_token)
+      self.class.default_params :auth_token => auth_token
+    end
+
+    def tickets(options={})
+      response = self.class.get("/tickets.json")
+      response.body
+    end
+  end
 end
+
--- supportbee.gemspec
+++ supportbee.gemspec
@@ -14,4 +14,6 @@ Gem::Specification.new do |gem|
   gem.name          = "supportbee"
   gem.require_paths = ["lib"]
   gem.version       = Supportbee::VERSION
+
+  gem.add_dependency "httparty"
 end
