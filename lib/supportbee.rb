78b71500ed1d012f88844ebbfeff4ca4b8c87ce9
Manoj Mishra <b0rn2c0d3@gmail.com>
2012-07-17 13:25:26 +0530

    find tickets by agent name

--- lib/supportbee.rb
+++ lib/supportbee.rb
@@ -11,15 +11,23 @@ module Supportbee
     end
 
     def tickets(options={})
-     valid_keys = [:per_page, :page, :spam, :trash, :replies, :max_replies, :assigned_user, :assigned_group, :starred, :label, :since, :until]
+     valid_keys = [:per_page, :page, :spam, :trash, :replies, :max_replies, :assigned_user, :assigned_group, :starred, :label, :since, :until,:agent]
      invalid_keys = options.keys - valid_keys
 
      raise "Invalid Options: #{invalid_keys.join(', ')}" unless invalid_keys.empty?
-
+     if options.fetch(:agent)
+       options[:assigned_user]= self.users(options[:agent])
+     end
      self.class.default_params.merge!(options)
+     response = self.class.get("/tickets.json")
+     response.body
 
-      response = self.class.get("/tickets.json")
-      response.body
+    end
+
+    def users(agent)            
+      response=self.class.get("/users.json")
+      users=JSON.parse(response.body)
+      users["users"].each { |user| return user['id'] if user['name']==agent}      
     end
   end
 end
