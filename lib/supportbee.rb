b16a414e2efec494351cfe3fd449f5b12ce7a4bf
Manoj Mishra <b0rn2c0d3@gmail.com>
2012-07-27 19:25:07 +0530

    added group

--- lib/supportbee.rb
+++ lib/supportbee.rb
@@ -11,12 +11,15 @@ module Supportbee
     end
 
     def tickets(options={})
-     valid_keys = [:per_page, :page, :spam, :trash, :replies, :max_replies, :assigned_user, :assigned_group, :starred, :label, :since, :until,:agent]
+     valid_keys = [:per_page, :page, :spam, :trash, :replies, :max_replies, :assigned_user, :assigned_group, :starred, :label, :since, :until,:agent,:group]
      invalid_keys = options.keys - valid_keys
 
      raise "Invalid Options: #{invalid_keys.join(', ')}" unless invalid_keys.empty?
-     if options.fetch(:agent)
-       options[:assigned_user]= self.users(options[:agent])
+     unless options[:agent].nil?
+       options[:assigned_user]= self.users(options[:agent])     
+     end
+     unless options[:group].nil?
+       options[:assigned_group]= self.groups(options[:group])
      end
      self.class.default_params.merge!(options)
      response = self.class.get("/tickets.json")
@@ -29,6 +32,14 @@ module Supportbee
       users=JSON.parse(response.body)
       users["users"].each { |user| return user['id'] if user['name']==agent}      
     end
+   
+    def groups(group)            
+      response=self.class.get("/groups.json")
+      groups=JSON.parse(response.body)
+      puts groups
+      groups["groups"].each { |group| return group if group['name']==group}      
+    end
+
   end
 end
 
